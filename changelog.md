8411a3c2e76aa31df18998aa1f86c98d37a79a2f
Kishor Thanth <kishor.thanth@techseria.com>
2019-11-18 20:40:30 +0530

    Fixed a response message of the validate coupon code functionality

--- changelog.md
+++ changelog.md
@@ -1,6 +1,13 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [1.0.2] - 2019-11-18
+
+### Fixed
+- Fixed a response message of the validate coupon code functionality
+
+
+
 ## [1.0.1] - 2019-?-?
 
 ### Added
--- dotstudiopro-subscription.php
+++ dotstudiopro-subscription.php
@@ -15,7 +15,7 @@
  * Plugin Name:       dotstudioPRO Subscription
  * Plugin URI:        https://www.dotstudiopro.com
  * Description:       This plugin is an addon to the dosstudioPRO API plugin to allow users to manage their subscriptions.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            dotstudioPRO
  * Author URI:        http://www.dotstudiopro.com
  * License:           GPLv3
